46ce06d4cbc3e2d233db4aed770ae4ba8bbf67d1
Erik Lange <erik.lange@fasttvnet.dk>
2023-03-11 17:07:12 +0100

    passes requests to controllers

--- tool/view_generator.py
+++ tool/view_generator.py
@@ -1,7 +1,11 @@
 import json
 
+# This script generates view functions for all operations in the OpenAPI definition
+print('Generating view functions based on OpenAPI definition...')
+print('\n')
 # write to a file
-with open('flaskr/api.py', 'w') as f:
+file_location = 'flaskr/api.py'
+with open(file_location, 'w') as f:
     f.write('from flask import Blueprint, request')
     f.write('\n')
     f.write('import flaskr.controllers.users_controller as users_controller')
@@ -18,13 +22,15 @@ with open('flaskr/api.py', 'w') as f:
         api_def = json.load(json_file)
         # iterate over paths
         for path in api_def['paths']:
-            print(path)
+            print(f'{path}')
+            print(f'    Writing view functions for operations on this path...')
+            print('\n')
             # iterate over methods
             for method in api_def['paths'][path]:
                 if method == 'parameters':
                     continue
 
-                print(f'    {method}')
+                print(f'        {method}...')
                 operation_def = api_def['paths'][path][method]
                 
                 # get operationId
@@ -41,15 +47,14 @@ with open('flaskr/api.py', 'w') as f:
                 f.write(f'def {operation_id}():')
                 f.write('\n')
                 f.write(f'    """{description}"""')
+                f.write('\n')
 
-                # if this operation has a request body, get it
-                if operation_def.get('requestBody') != None:
-                    f.write('\n')
-                    f.write('    body = request.get_json()')
-                    f.write('\n')
-                    f.write(f'    return {tag}_controller.{operation_id}(body)')
-                    f.write('\n')
-                else:
-                    f.write('\n')
-                    f.write(f'    return {tag}_controller.{operation_id}()')
-                    f.write('\n')
+                # pass request to controller
+                f.write(f'    return {tag}_controller.{operation_id}(request)')
+                f.write('\n\n')
+
+                print(f'        Successfully wrote view function!')
+                print('\n')
+    
+    print(f'Successfully wrote all view functions to {file_location}!')
+                
