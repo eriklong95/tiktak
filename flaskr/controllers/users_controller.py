834127367966b61fa9262ded9a2d4be517545db3
Erik Lange <erik.lange@fasttvnet.dk>
2023-03-11 17:14:21 +0100

    extracts body from POST request to /users

--- flaskr/controllers/users_controller.py
+++ flaskr/controllers/users_controller.py
@@ -1,10 +1,12 @@
-def users__get__list():
+def users__get__list(request):
     return "users__get__list"
 
 
-def users__post__create(body):
-    return "users__post__create"
+def users__post__create(request):
+    body = request.get_json()
+    username = body["username"]
+    return f"users__post__create: {username}"
 
 
-def some_user__get__info():
+def some_user__get__info(request):
     return "some_user__get__info"
