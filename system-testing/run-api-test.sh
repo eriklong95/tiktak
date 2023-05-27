ff0250991e7bc3301cb1d06d198d2a9141dadd9e
Erik Lange <erik.lange@fasttvnet.dk>
2023-05-27 09:03:53 +0200

    API test script takes test name as argument

--- system-testing/run-api-test.sh
+++ system-testing/run-api-test.sh
@@ -3,13 +3,12 @@ echo "This is a test of the tiktak API."
 # start tiktak in docker container
 docker run -p 1234:1234 -d --name tiktak_container tiktak
 
-# print stuff from the newman run for the user to see
 docker run -v $PWD/test-resources/postman:/etc/newman \
     --net host \
     --name newman_container \
     --platform linux/amd64/v8 \
     postman/newman \
-    run --env-var baseUrl=http://0.0.0.0:1234 collections/user-creation-test.postman_collection.json
+    run --env-var baseUrl=http://0.0.0.0:1234 collections/$1.postman_collection.json
 
 # stop and remove newman container
 docker stop newman_container
