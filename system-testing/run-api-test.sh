5502f6c3d6bdd46df362c6c40cda6538517977ad
Erik Lange <erik.lange@fasttvnet.dk>
2023-05-27 11:23:13 +0200

    makes scripts more user-friendly

--- system-testing/run-api-test.sh
+++ system-testing/run-api-test.sh
@@ -1,4 +1,8 @@
 echo "This is a test of the tiktak API."
+if [ $# -ne 1 ]; then
+    echo "Usage: sh run-api-test.sh [Postman collection JSON file name]"
+    exit 1
+fi
 
 # start tiktak in docker container
 docker run -p 1234:1234 -d --name tiktak_container tiktak
@@ -6,14 +10,15 @@ docker run -p 1234:1234 -d --name tiktak_container tiktak
 docker run -v $PWD/test-resources/postman:/etc/newman \
     --net host \
     --name newman_container \
-    --platform linux/amd64/v8 \
     postman/newman \
-    run --env-var baseUrl=http://0.0.0.0:1234 collections/$1.postman_collection.json
+    run --env-var baseUrl=http://0.0.0.0:1234 collections/$1
 
-# stop and remove newman container
 docker stop newman_container
+echo "... stopped."
 docker rm newman_container
+echo "... was removed."
 
-# stop and remove tiktak container
 docker stop tiktak_container
+echo "... stopped."
 docker rm tiktak_container
+echo "... was removed."
--- system-testing/run-performance-test.sh
+++ system-testing/run-performance-test.sh
@@ -1,4 +1,8 @@
-echo "This is a test of the tiktak API."
+echo "This is a performance test of the tiktak server."
+if [ $# -ne 1 ]; then
+    echo "Usage: sh run-performance-test.sh [k6 script file name]"
+    exit 1
+fi
 
 # start tiktak in docker container
 docker run -p 1234:1234 -d --name tiktak_container tiktak
@@ -7,10 +11,16 @@ docker run -v $PWD/test-resources/k6/scripts:/src \
     --net host \
     --name k6_container \
     grafana/k6 \
-    run -e BASE_URL=http://0.0.0.0:1234 /src/user-creation.js
+    run -e BASE_URL=http://0.0.0.0:1234 /src/$1
 
-docker stop tiktak_container
-docker rm tiktak_container
+echo "Wait for the Docker containers to stop and be removed"
 
 docker stop k6_container
+echo "... stopped."
 docker rm k6_container
+echo "... was removed."
+
+docker stop tiktak_container
+echo "... stopped."
+docker rm tiktak_container
+echo "... was removed."
