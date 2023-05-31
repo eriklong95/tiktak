50388315965e58d1fa432b034af88b63acb96990
Erik Lange <erik.lange@fasttvnet.dk>
2023-05-31 19:44:10 +0200

    Implement user endpoints.

--- src/flaskr/controllers/users_controller.py
+++ src/flaskr/controllers/users_controller.py
@@ -9,16 +9,20 @@ USER_SCHEMA = UserSchema()
 def users__get__list(request):
     repo = UserRepositoryApi()
     users = repo.select_all_users()
-    return make_response([USER_SCHEMA.dump(u) for u in users], 200)
+    return make_response([u.username for u in users], 200)
 
 
 def users__post__create(request):
-    # TODO: implement
-
-    return make_response('Not yet implemented', 500)
+    body = request.json
+    new_user = User(body, 0)
+    repo = UserRepositoryApi()
+    repo.insert(new_user)
+    repo.commit()
+    return make_response(USER_SCHEMA.dump(new_user), 201)
 
 
 def some_user__get__info(request, username):
-    # TODO: implement
+    repo = UserRepositoryApi()
+    user = repo.select_user(username)
+    return make_response(USER_SCHEMA.dump(user), 200)
 
-    return make_response('Not yet implemented', 500)
