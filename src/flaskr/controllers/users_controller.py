5b2565b45ac9d7985b7dd62dfe58883ab226e554
Erik Lange <erik.lange@fasttvnet.dk>
2023-05-31 19:58:28 +0200

    Fix user endpoint edge-cases

--- src/flaskr/controllers/users_controller.py
+++ src/flaskr/controllers/users_controller.py
@@ -16,6 +16,12 @@ def users__post__create(request):
     body = request.json
     new_user = User(body, 0)
     repo = UserRepositoryApi()
+
+    existing_user = repo.select_user(new_user.username)
+    
+    if (existing_user != None):
+        return make_response('A user with this username already exists', 403)
+
     repo.insert(new_user)
     repo.commit()
     return make_response(USER_SCHEMA.dump(new_user), 201)
@@ -24,5 +30,9 @@ def users__post__create(request):
 def some_user__get__info(request, username):
     repo = UserRepositoryApi()
     user = repo.select_user(username)
+
+    if (user == None):
+        return make_response('No such user exists', 404)
+    
     return make_response(USER_SCHEMA.dump(user), 200)
 
