31f543ee2e607a49e7d4789586d50687532db66e
Erik Lange <eriklange.cph@proton.me>
2026-02-22 12:50:28 +0100

    removes swagger ui

--- server/src/flaskr/__init__.py
+++ server/src/flaskr/__init__.py
@@ -1,7 +1,10 @@
 from logging import config
+
 from flask import Flask, make_response, redirect
-from flask_swagger_ui import get_swaggerui_blueprint
-from src.flaskr.persistence.db_connection.db_connection_supplier import DatabaseConnectionSupplier
+
+from src.flaskr.persistence.db_connection.db_connection_supplier import (
+    DatabaseConnectionSupplier,
+)
 
 
 def create_app():
@@ -16,70 +19,66 @@ def create_app():
 
 
 def configure_logging():
-    config.dictConfig({
-        'version': 1,
-        'formatters': {'default': {
-            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-        }},
-        'handlers': {'wsgi': {
-            'class': 'logging.StreamHandler',
-            'stream': 'ext://flask.logging.wsgi_errors_stream',
-            'formatter': 'default'
-        }},
-        'root': {
-            'level': 'DEBUG',
-            'handlers': ['wsgi']
+    config.dictConfig(
+        {
+            "version": 1,
+            "formatters": {
+                "default": {
+                    "format": "[%(asctime)s] %(levelname)s in %(module)s: %(message)s",
+                }
+            },
+            "handlers": {
+                "wsgi": {
+                    "class": "logging.StreamHandler",
+                    "stream": "ext://flask.logging.wsgi_errors_stream",
+                    "formatter": "default",
+                }
+            },
+            "root": {"level": "DEBUG", "handlers": ["wsgi"]},
         }
-    })
+    )
 
 
 def initialize_database(logger):
-    logger.info('Initializing database')
+    logger.info("Initializing database")
 
     connection = DatabaseConnectionSupplier().get()
     cursor = connection.cursor()
 
-    all_tables_result = cursor.execute('SELECT name FROM sqlite_master')
+    all_tables_result = cursor.execute("SELECT name FROM sqlite_master")
     tables = all_tables_result.fetchall()
 
-    user_table_exists = any(list(filter(lambda t: t[0] == 'user', tables)))
+    user_table_exists = any(list(filter(lambda t: t[0] == "user", tables)))
     if not user_table_exists:
-        cursor.execute(
-            'CREATE TABLE user(username TEXT PRIMARY KEY, rank INTEGER)')
-        logger.info('Created table \'user\'')
+        cursor.execute("CREATE TABLE user(username TEXT PRIMARY KEY, rank INTEGER)")
+        logger.info("Created table 'user'")
 
-    game_table_exists = any(list(filter(lambda t: t[0] == 'game', tables)))
+    game_table_exists = any(list(filter(lambda t: t[0] == "game", tables)))
     if not game_table_exists:
         cursor.execute(
-            'CREATE TABLE game(id TEXT PRIMARY KEY, player_a TEXT, player_b TEXT, FOREIGN KEY (player_a) REFERENCES user (username), FOREIGN KEY (player_b) REFERENCES user (username))')
-        logger.info('Created table \'game\'')
+            "CREATE TABLE game(id TEXT PRIMARY KEY, player_a TEXT, player_b TEXT, FOREIGN KEY (player_a) REFERENCES user (username), FOREIGN KEY (player_b) REFERENCES user (username))"
+        )
+        logger.info("Created table 'game'")
 
-    move_table_exists = any(list(filter(lambda t: t[0] == 'move', tables)))
+    move_table_exists = any(list(filter(lambda t: t[0] == "move", tables)))
     if not move_table_exists:
         cursor.execute(
-            'CREATE TABLE move(x INTEGER, y INTEGER, occupier TEXT, game_id TEXT, PRIMARY KEY (game_id, x, y), FOREIGN KEY (game_id) REFERENCES game (id) ON DELETE CASCADE)')
-        logger.info('Created table \'move\'')
+            "CREATE TABLE move(x INTEGER, y INTEGER, occupier TEXT, game_id TEXT, PRIMARY KEY (game_id, x, y), FOREIGN KEY (game_id) REFERENCES game (id) ON DELETE CASCADE)"
+        )
+        logger.info("Created table 'move'")
 
 
 def configure_routing(app):
-    @app.get('/')
+    @app.get("/")
     def index():
-        return redirect('/static/index.html', 302)
+        return redirect("/static/index.html", 302)
 
-    @app.get('/test')
+    @app.get("/test")
     def test():
-        return make_response('You called the tiktak server and had a successful response', 200)
-
-    SWAGGER_URL = '/docs'
-    API_URL = '/static/openapi.json'
-    swaggerui_blueprint = get_swaggerui_blueprint(
-        SWAGGER_URL,
-        API_URL,
-        config={
-            'app_name': "tiktak"
-        }
-    )
-    app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+        return make_response(
+            "You called the tiktak server and had a successful response", 200
+        )
 
     from . import api
+
     app.register_blueprint(api.bp)
